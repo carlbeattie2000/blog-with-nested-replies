fa825eca0eb0db0ad69125544840b704d041dd83
carlbeattie2000 <12beattiecastp@gmail.com>
2021-11-01 14:01:34 +0000

    Removed Upvote feature(doesn't fit in, added view post page, add reply feature)

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-const express = require('express');
-const path = require('path');
-var sqlite3 = require('sqlite3').verbose();
-const bodyParser = require('body-parser');
-var crypto = require("crypto");
+const express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    PORT = process.env.PORT || 3000;
+var sqlite3 = require('sqlite3').verbose(),
+    crypto = require("crypto"),
+    session = require('express-session')
 
-const PORT = process.env.PORT || 3000;
-
+// App 
 const app = express();
 app.set('view engine', 'pug')
 app.use(bodyParser.urlencoded({ extended: false }))
+app.set('trust proxy', 1) // trust first proxy
+app.use(bodyParser.json());
+// Session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
 
+// DB init
 var db = new sqlite3.Database('./db/blogs.db');
 
+// CREATE DATABASE TABLES
 db.run("CREATE TABLE IF NOT EXISTS main_posts (post_id TEXT, title TEXT, content TEXT, important INTEGER, upvotes INTEGER, replys_count INTEGER, time_posted TEXT, deletion_date TEXT)", (err) => {
     if (err) throw (error)
 });
@@ -19,10 +31,9 @@ db.run("CREATE TABLE IF NOT EXISTS posts_replys (post_id TEXT, comment TEXT, upv
     if (err) throw (error)
 });
 db.run("CREATE TABLE IF NOT EXISTS admins (account_id TEXT, username TEXT, password TEXT)", (err) => {
-    if (err) throw (error);
-})
-
-app.use(bodyParser.json());
+        if (err) throw (error);
+    })
+// END CREATE DATABASE TABLES
 
 app.get("/", (req, res) => { // Main home page, where all recent posts are displayed
     var posts_list = []; // Store all the posts inside here
@@ -35,7 +46,7 @@ app.get("/", (req, res) => { // Main home page, where all recent posts are displ
                 content: row.content,
                 important: row.important,
                 upvotes: row.upvotes,
-                replys_count : row.replys_count,
+                replys_count: row.replys_count,
                 time_posted: row.time_posted,
                 deletion_data: row.deletion_date
             }
@@ -43,7 +54,7 @@ app.get("/", (req, res) => { // Main home page, where all recent posts are displ
             posts_list.push(post)
         })
 
-        res.render("index", {postlist: posts_list})
+        res.render("index", { postlist: posts_list })
     })
 })
 
@@ -57,14 +68,14 @@ app.post("/create_post/send", (req, res) => {
     var post_id = crypto.randomBytes(20).toString('hex');
     var post_important = req.body.importantCheck;
     var date = new Date();
-    var date_added = String(date.getDate()) + "/" + String(date.getMonth()+1) + "/" + String(date.getFullYear()) + " " + String(date.getHours()) + ":" + String(date.getMinutes());
+    var date_added = String(date.getDate()) + "/" + String(date.getMonth() + 1) + "/" + String(date.getFullYear()) + " " + String(date.getHours()) + ":" + String(date.getMinutes());
 
     if (post_important == "1") {
         post_important = true;
-        var deletion_time = String(date.getDate() + 4) + String(date.getMonth()+1) + String(date.getHours()) + String(date.getMinutes());
+        var deletion_time = String(date.getDate() + 4) + String(date.getMonth() + 1) + String(date.getHours()) + String(date.getMinutes());
     } else {
         post_important = false;
-        var deletion_time = String(date.getDate() + 1) + String(date.getMonth()+1) + String(date.getHours()) + String(date.getMinutes());
+        var deletion_time = String(date.getDate() + 1) + String(date.getMonth() + 1) + String(date.getHours()) + String(date.getMinutes());
     }
 
     if (post_title && post_content) {
@@ -80,7 +91,105 @@ app.post("/create_post/send", (req, res) => {
     }
 })
 
+app.post("/view_post", (req, res) => {
+    const post_id = req.body.post_id;
+    var replys = [];
+    var posts = []
+        // get the post and store it in a dict
+    db.all(`SELECT * FROM main_posts WHERE post_id="${post_id}"`, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        rows.forEach((row) => {
+            var post = {
+                post_id: row.post_id,
+                title: row.title,
+                content: row.content,
+                important: row.important,
+                upvotes: row.upvotes,
+                replys_count: row.replys_count,
+                time_posted: row.time_posted,
+                deletion_data: row.deletion_date
+            }
 
+            posts.push(post)
+        });
+
+        db.all(`SELECT * FROM posts_replys WHERE post_id="${post_id}"`, [], (err, rows) => {
+            if (err) {
+                throw err;
+            }
+            rows.forEach((row) => {
+                var reply = {
+                    post_id: row.post_id,
+                    comment: row.comment,
+                    upvotes: row.upvotes,
+                    time_posted: row.time_posted,
+                    reply_id: row.reply_id
+                }
+
+                replys.push(reply);
+            });
+
+            res.render("view_post", { postlist: posts, replylist: replys })
+        });
+    });
+})
+
+app.post("/reply/send", (req, res) => {
+    const post_id = req.body.post_id
+    const comment = req.body.reply_content
+    var reply_id = crypto.randomBytes(20).toString('hex');
+    var date = new Date();
+    var replys = [];
+    var posts = [];
+    var date_added = String(date.getDate()) + "/" + String(date.getMonth() + 1) + "/" + String(date.getFullYear()) + " " + String(date.getHours()) + ":" + String(date.getMinutes());
+    if (post_id && comment) {
+        db.run("INSERT INTO posts_replys(post_id, comment, upvotes, time_posted, reply_id) VALUES (?, ?, ?, ?, ?)", [post_id, comment, 0, date_added, reply_id], (err) => {
+            if (err) throw error;
+            db.all(`SELECT * FROM main_posts WHERE post_id="${post_id}"`, [], (err, rows) => {
+                if (err) {
+                    throw err;
+                }
+                rows.forEach((row) => {
+                    var post = {
+                        post_id: row.post_id,
+                        title: row.title,
+                        content: row.content,
+                        important: row.important,
+                        upvotes: row.upvotes,
+                        replys_count: row.replys_count,
+                        time_posted: row.time_posted,
+                        deletion_data: row.deletion_date
+                    }
+
+                    posts.push(post)
+                    var new_reply_count = post.replys_count + 1;
+                    db.all(`UPDATE main_posts SET replys_count=${new_reply_count} WHERE post_id="${post.post_id}"`)
+                });
+
+                db.all(`SELECT * FROM posts_replys WHERE post_id="${post_id}"`, [], (err, rows) => {
+                    if (err) {
+                        throw err;
+                    }
+                    rows.forEach((row) => {
+                        var reply = {
+                            post_id: row.post_id,
+                            comment: row.comment,
+                            upvotes: row.upvotes,
+                            time_posted: row.time_posted,
+                            reply_id: row.reply_id
+                        }
+
+                        replys.push(reply);
+                    });
+
+                    res.render("view_post", { postlist: posts, replylist: replys })
+                });
+            });
+        })
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Listing on port ${PORT}`);
